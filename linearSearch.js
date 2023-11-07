1668205fdf4950d636ffa3c0a39ff791c0831634
Lukmanam <lukkuptrklm2000@gmail.com>
2023-11-07 11:04:33 +0530

    linearSearch

--- linearSearch.js
+++ linearSearch.js
@@ -1,12 +1,10 @@
 
 
-function linearSearch(arr,target){
+function linearSearch(arr, target) {
 
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i]===target)
-        {
-                return  i
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i
         }
     }
     return -1;
@@ -14,14 +12,12 @@ function linearSearch(arr,target){
 }
 
 
-let arr=[2,4,1,68,68,35]
-const result=linearSearch(arr,68)
-if(result!=-1)
-{
+let arr = [2, 4, 1, 68, 68, 35]
+const result = linearSearch(arr, 68)
+if (result != -1) {
     console.log(`found ${result}`);
 }
-else
-{
+else {
     console.log("not found");
 }
 
