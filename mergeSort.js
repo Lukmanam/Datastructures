74b2fd34f81d2bad3341f12a7263d74f019d3f96
Lukmanam <lukkuptrklm2000@gmail.com>
2023-11-07 20:46:16 +0530

    updation in mergesort

--- mergeSort.js
+++ mergeSort.js
@@ -1,39 +1,38 @@
-// function mergesort(a)
-// {
-//     if(a.length<2)
-//     {
-//         return a
-//     }
-//     let middle=Math.floor(a.length/2)
-//     let lefta=a.slice(0,middle);
-//     let righta=a.slice(middle);
+function mergesort(a) {
+    if (a.length < 2) {
+        return a
+    }
+    let middle = Math.floor(a.length / 2)
+    let lefta = a.slice(0, middle);
+    let righta = a.slice(middle);
 
 
-//    return merge(mergesort(lefta),mergesort(righta))
+    return merge(mergesort(lefta), mergesort(righta))
 
 
-// function merge(lefta,righta)
-// {
-//     let sorteda=[]
-//     while(lefta.length && righta.length )
-//     {
-//         if(lefta[0]<righta[0])
-//         {
-//             sorteda.push(lefta.shift())
-//         }
-//         else
-//         {
-//             sorteda.push(righta.shift())
-//         }
-//     }
-//     return [...sorteda,...lefta,...righta]
+    function merge(lefta, righta) {
+        let sorteda = []
+        while (lefta.length && righta.length) {
+            if (lefta[0] < righta[0]) {
+                sorteda.push(lefta.shift())
+            }
+            else {
+                sorteda.push(righta.shift())
+            }
+        }
+        return [...sorteda, ...lefta, ...righta]
 
 
-// } 
-// }
-// let a=[12,34,56,13,33,99,22]
-// console.log(mergesort(a));
+    }
+}
+let a = [12, 34, 56, 13, 33, 99, 22]
+console.log(mergesort(a));
 
+
+
+
+
+// Using Arrow function
 
 const mergesorted = ((a) => {
 
@@ -59,6 +58,6 @@ const merge = ((lefta, righta) => {
     return [...sorteda, ...lefta, ...righta]
 })
 
-let a = [12, 45, 2, 67, 34, 68, 32]
+
 console.log(mergesorted(a));
 
