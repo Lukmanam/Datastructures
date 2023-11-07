34daa81c91c51ffc28d21c780444ea7a59fa0924
Lukmanam <lukkuptrklm2000@gmail.com>
2023-11-07 10:40:09 +0530

    likedlist search and fetch element

--- linkedList.js
+++ linkedList.js
@@ -18,83 +18,111 @@ class linkedList {
             this.head = node
             this.tail = node
             this.size++;
-            
+
         }
         else {
             this.tail.next = node
             this.tail = node;
             this.size++;
-            
+
         }
 
     }
-    Prepend(value)
-    {
-        const node=new Node(value)
+    Prepend(value) {
+        const node = new Node(value)
         if (!this.head) {
             this.head = node
             this.tail = node
             this.size++;
-            
+
         }
-        else
-        {
-            node.next=this.head
-            this.head=node;
+        else {
+            node.next = this.head
+            this.head = node;
             this.size++;
         }
     }
 
-    insert(value,index)
-    {
-        if(index<0 || index>this.size)
-        {
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
             console.log("error");
         }
-        const node=new Node(value)
-        let current=this.head
-        if(index===0)
-        {
+        const node = new Node(value)
+        let current = this.head
+        if (index === 0) {
             this.Prepend(value)
         }
-        for (let i=0;i<index-1;i++)
-        {
-            current=current.next
-            
+        for (let i = 0; i < index - 1; i++) {
+            current = current.next
+
 
         }
-        node.next=current.next
-        current.next=node
+        node.next = current.next
+        current.next = node
         this.size++;
     }
 
-    remove(index)
-    {
+    remove(index) {
         let removenode;
-        if(index<0 || index>this.size)
-        {
+        if (index < 0 || index > this.size) {
             console.log("error");
         }
-        if(index===0)
-        {
-            removenode=this.head;
-            this.head=this.head.next
+        if (index === 0) {
+            removenode = this.head;
+            this.head = this.head.next
             this.size--;
         }
-        else
+        else {
+
+            let current = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                current = current.next
+            }
+            removenode = current.next
+            current.next = removenode.next
+            this.size--;
+
+        }
+
+    }
+
+
+    // fetch value from a particular index
+    fetch(index) {
+        let current = this.head
+        let flag = 0;
+        for (let i = 1; i <= index; i++) {
+            if (i === index) {
+                console.log(current.value);
+                flag++;
+
+            }
+            current = current.next
+        }
+        if (flag === 0) {
+            console.log("Not found");
+        }
+
+    }
+
+
+    // Search value in linked list
+    search(value)
+    {
+        let current=this.head
+        let flag=0
+        for(let i=1;i<this.size;i++)
         {
-            
-            let current=this.head;
-            for(let i=0;i<index-1;i++)
+            if(current.value===value)
             {
-                current=current.next
-            } 
-            removenode=current.next
-            current.next=removenode.next
-            this.size--;
-
+                console.log(`found at ${i} nd node`);
+                flag++;
+            }
+            current=current.next
         }
-
+        if(flag===0){
+            console.log("element u r searching for not found");
+        }
     }
 
 
@@ -107,14 +135,19 @@ class linkedList {
         }
     }
 }
-const list=new linkedList()
+
+
+const list = new linkedList()
 list.append(9)
 list.append(777)
 list.Prepend(8888)
-list.insert(999,2)
-
-list.insert(1020,3)
-list.insert(10044,5)
+list.insert(999, 2)
+list.insert(1020, 3)
+list.insert(10044, 5)
 list.remove(5)
+list.fetch(1)
+list.search(9)
+
+
 
 list.display()
