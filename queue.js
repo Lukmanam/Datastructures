0cb51e45f238ee96a09a9bc0725a4b2d59eba6dc
Lukmanam <lukkuptrklm2000@gmail.com>
2023-11-12 23:41:09 +0530

    formatted code

--- queue.js
+++ queue.js
@@ -1,39 +1,36 @@
-class Queue{
-    constructor(){
-        this.items=[]
+class Queue {
+    constructor() {
+        this.items = []
     }
-    enqueue(elemnt)
-    {
+    enqueue(elemnt) {
         this.items.push(elemnt)
     }
-    dequeue(){
-        if(this.isEmpty()){
+    dequeue() {
+        if (this.isEmpty()) {
             return "Underflow"
         }
         return this.items.shift();
     }
-    front()
-    {
-        if(this.isEmpty()){
+    front() {
+        if (this.isEmpty()) {
             return "No elements in the queue"
         }
         return this.items[0]
     }
-    isEmpty(){
-        return this.items.lenght===0;
+    isEmpty() {
+        return this.items.lenght === 0;
     }
-    printqueue(){
-        let str="";
-        for(let i=0;i<this.items.length;i++)
-        {
-            str+=this.items[i]+ "";
+    printqueue() {
+        let str = "";
+        for (let i = 0; i < this.items.length; i++) {
+            str += this.items[i] + "";
         }
         return str;
     }
 }
 
 
-const queue= new Queue();
+const queue = new Queue();
 queue.enqueue(10);
 queue.enqueue(20);
 queue.enqueue(400)
